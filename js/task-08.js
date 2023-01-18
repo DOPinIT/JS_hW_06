69819baa706b9f6bc1a2e326c3ab487901cd1d41
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-01-19 00:50:14 +0200

    fixForm_hw6

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,13 @@
 const formLogin = document.querySelector("form.login-form");
 
-formLogin.addEventListener("submit", onformSubmit);
-
-function onformSubmit(event) {
+formLogin.addEventListener("submit",(event) => {
   event.preventDefault();
-
-  const formEls = event.currentTarget.elements;
-
-  if (formEls.email.value === "" || formEls.password.value === "") {
-    alert("Всі поля мають бути заповнені!");
+  const { elements: { email, password } } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+      alert("Всі поля повинні бути заповнені!");
   } else {
-    console.log(`Email: ${formEls.email.value}, Password: ${formEls.password.value}`);
-    event.currentTarget.reset();
+      console.log({ "email": email.value, "password": password.value });
+      formLogin.reset();
   }
-}
+});
+
